4cc9a466771d0a6d9565be0e1f7708d68bdedf9a
genesis-g101 <gyreplicant@gmail.com>
2023-09-19 17:02:32 -0300

    terminal responsivo e mobile

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const texts = [
     "<span class='terminal-text__prompt'>Apenas uma entusiata de tecnologia da informação, que gosta de resolver problemas de forma criativa!</span>",
     "<span class='terminal-text__prompt'>Adoro jogos que envolvem estrategia e resolução de puzzles.</span>",
     "<span class='terminal-text__prompt'>No momento estudo a linguagem C e como funciona o sistema operacional.</span>",
-    "<span class='terminal-text__prompt'>____________________________________</span>",
+    "<span class='terminal-text__prompt'>---------------------------------</span>",
     "<span class='terminal-text__prompt'>Veja meus <a href='/projetos' target='_blank'>projetos</a> e <a href='https://medium.com/@g101' target='_blank'>blog</a>.",
     "<span class='terminal-text__prompt'>Você pode entrar em contato comigo no <a href='https://www.linkedin.com/in/genesislima101/' target='_blank'>linkedin</a>.",
 ];
