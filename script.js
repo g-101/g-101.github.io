37ca6f078c9bbe51405d29967c0e07041d9bfcf3
genesis-g101 <gyreplicant@gmail.com>
2024-01-24 22:05:39 -0300

    corrigindo erro de gramatica

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const texts = [
     "<span class='terminal-text__prompt'>Hello world! Bem vinda(o) ao meu portfólio. Meu nome é <span class='highlight'>Genesis</span>.</span>",
-    "<span class='terminal-text__prompt'>Apenas uma entusiata que adora programar e resolver problemas de forma criativa!</span>",
+    "<span class='terminal-text__prompt'>Apenas uma entusiasta que adora programar e resolver problemas de forma criativa!</span>",
     "<span class='terminal-text__prompt'>No momento, sou aluna do curso profissionalizante CyberOps Associate da Cisco em parceria com o Senai.</span>",
     "<span class='terminal-text__prompt'>---------------------------------</span>",
     "<span class='terminal-text__prompt'>Veja meus <a href='/projetos' target='_blank'>projetos</a> e <a href='https://medium.com/@g101' target='_blank'>blog</a>.",
