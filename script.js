2fbe3365e8208dcab8b46b39cb240feceda5b44b
genesis-g101 <gyreplicant@gmail.com>
2024-01-25 21:59:27 -0300

    att home e projetos: 25/01

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const texts = [
     "<span class='terminal-text__prompt'>Hello world! Bem vinda(o) ao meu portfólio. Meu nome é <span class='highlight'>Genesis</span>.</span>",
-    "<span class='terminal-text__prompt'>Apenas uma entusiasta que adora programar e resolver problemas de forma criativa!</span>",
+    "<span class='terminal-text__prompt'>Apenas uma entusiasta em sistemas Linux, que adora programar e resolver problemas de forma criativa!</span>",
     "<span class='terminal-text__prompt'>No momento, sou aluna do curso profissionalizante CyberOps Associate da Cisco em parceria com o Senai.</span>",
     "<span class='terminal-text__prompt'>---------------------------------</span>",
-    "<span class='terminal-text__prompt'>Veja meus <a href='/projetos' target='_blank'>projetos</a> e <a href='https://medium.com/@g101' target='_blank'>blog</a>.",
+    "<span class='terminal-text__prompt'>Veja meus <a href='/projetos' target='_blank'>projetos</a> e <a href='https://g-101.github.io/elas-na-tech-2023/curriculo/' target='_blank'>currículo</a>.",
     "<span class='terminal-text__prompt'>Você pode entrar em contato comigo no <a href='https://www.linkedin.com/in/genesislima101/' target='_blank'>linkedin</a>.",
 ];
 
